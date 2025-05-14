a55b01ee617b822d4218c3483a38af926c803ff1
Just22py <si22eun@gmail.com>
2025-05-14 15:07:40 +0900

    Create 0514_23969버블정렬2.py

---
+++ 0514_23969버블정렬2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline 
+
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+result = -1
+cnt = 0 
+
+for i in range(N-1, 0, -1):
+    for j in range(i):
+        if arr[j] > arr[j +1]:
+            cnt += 1
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            if cnt == K:
+                print(*arr)
+                sys.exit()
+
+print(-1)
